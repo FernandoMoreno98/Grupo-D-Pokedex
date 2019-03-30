5443852c2c4fe5d0eed55f77e123d22a04792963
lmml- <43039968+lmml-l@users.noreply.github.com>
2019-03-30 17:47:17 +0100

    Escenas
    Se suben las distintas escenas

---
+++ pokedex/codigo/VistaSeleccionGeneral.js
@@ -0,0 +1,16 @@
+class VistaSeleccionGeneral extends Escena{
+	
+	constructor(plantilla){
+		this.plantilla = plantilla;
+		this.indicePos = new Array(7);
+		this.posActual = 0;
+
+	}
+	pintar(fondo){
+		var canvas=document.getElementById("canvas");   
+    	var canvas2d=canvas.getContext("2d");
+    	canvas2d.clearRect(0, 0, canvas.width, canvas.height);
+    	canvas2d.drawImage(document.getElementById(fondo),10,8,canvas.width,canvas.height);
+    	canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-20,canvas.height);
+	}
+}
