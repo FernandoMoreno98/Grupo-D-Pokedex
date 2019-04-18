bcbfb9c1673e52a5065765cad287b54e76cf8f08
lmml- <43039968+lmml-l@users.noreply.github.com>
2019-04-18 17:49:59 +0200

    Cambios en el target  y App

--- es.urjc.pokedex/src/main/java/Grupo_D/Pokedex/App.java
+++ es.urjc.pokedex/src/main/java/Grupo_D/Pokedex/App.java
@@ -1,17 +1,28 @@
 package Grupo_D.Pokedex;
 
+import org.bson.BsonDocument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class App 
 {
-	public static void main(String[] args) {
-		MongoClient mongoClient = new MongoClient("localhost", 8080);
+	public static void main(String[] args) {				
+		 MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+	     MongoClient client = new MongoClient("localhost", options);
+	     
+	     MongoDatabase db = client.getDatabase("test").withReadPreference(ReadPreference.secondary());
+	     MongoCollection<BsonDocument> coll = db.getCollection("test", BsonDocument.class);
+	     
 		 SpringApplication.run(App.class, args);
 	}
 }
