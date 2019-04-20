73e801eeb58ca0d825cae6afce5841cc70301af5
lmml- <43039968+lmml-l@users.noreply.github.com>
2019-04-20 11:42:20 +0200

    Vista tipos mejora

--- es.urjc.pokedex/src/main/resources/static/pokedex/codigo/ManejadorDeEscenas.js
+++ es.urjc.pokedex/src/main/resources/static/pokedex/codigo/ManejadorDeEscenas.js
@@ -190,12 +190,13 @@ class ManejadorDeEscenas {
         }else*/ 
         if(this.escena == this.vistaSeleccionTipos){
             //Poner el array de la lista con el string info recibido
-            var this = that;
+            var that = this;
             this.escena.EnterPulsado(
                 function(data){
-                    that.vistaPrincipal.listadePokemones = JSON.parse(info);
+                    that.vistaPrincipal.listadePokemones = JSON.parse(data);
                     //Ajustar indice de posicion
                     that.vistaPrincipal.posActualX=0;
+                    that.vistaPrincipal.posActualY=0;
 
                     that.escena = that.vistaPrincipal;
                     that.escenaAnterior = that.vistaSeleccionTipos;
