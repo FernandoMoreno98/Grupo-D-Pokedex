8dcf80bcd3dc1707acdc10ae4ddc2a02a0ee8d19
lmml- <43039968+lmml-l@users.noreply.github.com>
2019-04-20 14:44:04 +0200

    Autoajuste de plantilla y algunos datos

--- es.urjc.pokedex/target/classes/static/pokedex/codigo/VistaPrincipal.js
+++ es.urjc.pokedex/target/classes/static/pokedex/codigo/VistaPrincipal.js
@@ -5,7 +5,19 @@ class VistaPrincipal extends Escena{
 		this.plantilla = plantilla;
 		this.indicePos = new Array(7);
 
-        this.campos = []
+        this.campos = ["type1","type2",
+        "classfication","experience_growth",
+        "base_egg_steps"," ",//medios
+        "abilities",//largos
+        "against_bug", "against_dark", "against_dragon", "against_electric", 
+        "against_fairy", "against_fight", "against_fire", "against_flying", 
+        "against_ghost", "against_grass", "against_ground", "against_ice", 
+        "against_normal", "against_poison", "against_psychic", "against_rock", 
+        "against_steel", "against_water", "attack","base_happiness",
+        "base_total","capture_rate","defense","height_m",
+        "hp", "percentage_male", "sp_attack", "sp_defense",
+        "speed", "weight_kg", "generation", "is_legendary"]
+
         this.posActualX = 0;
 		this.posActualY = 0;
         //Array de pokemones que se recibe de la base de datos de mongo
@@ -16,7 +28,7 @@ class VistaPrincipal extends Escena{
     PintarDatosNoFijos(){
         var canvas=document.getElementById("canvas");   
         var canvas2d=canvas.getContext("2d");
-
+            
         var posActualenY = this.posActualY;
         var ArrayPosPokemonesPintar = new Array(6);
 
@@ -28,24 +40,84 @@ class VistaPrincipal extends Escena{
             var posMenosDos = posActualenY-2+i;
             if(posMenosDos < 0){
                 ArrayPosPokemonesPintar[i]=this.listadePokemones[this.listadePokemones.length + posMenosDos];
-                //---PROVISIONAL//
-                canvas2d.fillText(ArrayPosPokemonesPintar[i].type1 , 30 ,150+i*50);
-                //---PROVISIONAL//
+                console.log(ArrayPosPokemonesPintar[i])
+               
             }
             else if(posMenosDos>=this.listadePokemones.length){
                 ArrayPosPokemonesPintar[i]=this.listadePokemones[posMenosDos% (this.listadePokemones.length)];
-                //---PROVISIONAL//
-                canvas2d.fillText(ArrayPosPokemonesPintar[i].type1 , 30 ,160+i*50);
-                //---PROVISIONAL//
+                console.log(ArrayPosPokemonesPintar[i])
             }
             else{
                 ArrayPosPokemonesPintar[i]=this.listadePokemones[posMenosDos];
-                //---PROVISIONAL//
-                canvas2d.fillText(ArrayPosPokemonesPintar[i].type1 , 30 ,160+i*50);
-                //---PROVISIONAL//
+                console.log(ArrayPosPokemonesPintar[i])
             }
-            
         }
+
+        var campoActual = this.campos[this.posActualX];
+        var longitudDelcampo = this.ComprobadorLongitudCampo(campoActual);
+
+                if(longitudDelcampo==0){//corta plantilla
+                    this.plantilla = "listacorto";
+                    canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-10,canvas.height);
+
+                    canvas2d.fillText(this.campos[this.posActualX],100,100);
+                    canvas2d.fillText(this.campos[this.posActualX+1],100,100);
+                    canvas2d.fillText(this.campos[this.posActualX+2],100,100);
+                    canvas2d.fillText(this.campos[this.posActualX+3],100,100);
+
+                    for(var i = 0; i< ArrayPosPokemonesPintar.length ; i++){
+
+                        var pokemon = ArrayPosPokemonesPintar[i];
+
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX]],100,100);
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX+1]],100,100);
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX+2]],100,100);
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX+3]],100,100);
+
+                        canvas2d.fillText(pokemon.pokedex_number,100,100);
+                        canvas2d.fillText(pokemon.name,100,150);
+                    }
+
+                }else if(longitudDelcampo==1){//media plantilla
+                    this.plantilla = "listamedio";
+                    canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-10,canvas.height);
+
+
+                    canvas2d.fillText(this.campos[this.posActualX],100,100);
+                    canvas2d.fillText(this.campos[this.posActualX+1],100,100);
+
+                    for(var i = 0; i< ArrayPosPokemonesPintar.length ; i++){
+
+                        //console.log(ArrayPosPokemonesPintar[i].type1);
+                       
+                        var pokemon = ArrayPosPokemonesPintar[i];
+
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX]],300,200+i*20);
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX+1]],400,200+i*20);
+
+                        canvas2d.fillText(pokemon.pokedex_number,150,200+i*20);
+                        canvas2d.fillText(pokemon.name,100,200+i*20);
+                    }
+
+                }else{//larga plantilla
+                    this.plantilla = "listalargo";
+                    canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-10,canvas.height);
+
+                    canvas2d.fillText(this.campos[this.posActualX],100,100);
+
+                    for(var i = 0; i< ArrayPosPokemonesPintar.length ; i++){
+
+                        var pokemon = ArrayPosPokemonesPintar[i];
+
+                        canvas2d.fillText(pokemon[this.campos[this.posActualX]],100,100);
+
+                        canvas2d.fillText(pokemon.pokedex_number,100,100);
+                        canvas2d.fillText(pokemon.name,100,150);
+                    }
+                }   
+        
+                //canvas2d.fillText(ArrayPosPokemonesPintar[i].pokedex_number,100,100);
+                //canvas2d.fillText(ArrayPosPokemonesPintar[i].name,100,150);               
 
     }
 
@@ -56,8 +128,9 @@ class VistaPrincipal extends Escena{
     	var canvas2d=canvas.getContext("2d");
     	canvas2d.clearRect(0, 0, canvas.width, canvas.height);
     	canvas2d.drawImage(document.getElementById(this.fondo),10,8,canvas.width,canvas.height);
-    	canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-10,canvas.height);
+    	//canvas2d.drawImage(document.getElementById(this.plantilla),10,8,canvas.width-10,canvas.height);
 
+        this.PintarDatosNoFijos();
     	//Textos Base//
     	canvas2d.font= 'bold 20px "NeogreyMedium"';
     	canvas2d.fillText("LISTA TOTAL DE POKÉMON" , 40 , 40);
@@ -86,7 +159,6 @@ class VistaPrincipal extends Escena{
     	canvas2d.font= '16px "NeogreyMedium"';
     	canvas2d.fillText("Pokémon" , 760 ,40);
 
-        this.PintarDatosNoFijos();
 	}
 
     ArrowUpPulsado(){
@@ -123,5 +195,16 @@ class VistaPrincipal extends Escena{
 
     RPulsado(){
         console.log("Se ha pulsado R");
+    }
+
+    ComprobadorLongitudCampo(campo){
+        if(campo == "type1" || campo == "type2" ||campo == "classfication" ||campo == "experience_growth" //medios
+            || campo == "base_egg_steps" || campo == " " ){
+            return 1;
+        }else if(campo == "abilities"){//largos
+            return 2;
+        }else{//cortos
+            return 0;
+        }
     }
 }
